384941f389d7cee5412dc28a0dddb67454e6e8a5
Macaya25 <afmacaya@miuandes.cl>
2024-08-04 17:49:57 -0400

    Initial commit.

---
+++ lib/crud_app_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule CrudAppWeb.PageController do
+  use CrudAppWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
